11d57e97d378c364832f4f6388e8e0a782b81155
Gerardo Barcenas Jr <gbarcenasjr@gmail.com>
2021-02-26 23:33:26 -0800

    Added more navigation options

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Gerardo Barcenas Jr. on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
